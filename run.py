df5707b848f31d744de21458c569f307ee2aa6cb
NikkiGetsee <Nicola.getsee@gmail.com>
2024-05-03 18:01:10 +0000

    username request and card values added

---
+++ run.py
@@ -0,0 +1,17 @@
+import random
+ 
+# Global variables
+card_categories = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+cards_list = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+deck = [(card, category) for category in card_categories for card in cards_list]
+ 
+def card_value(card):
+    if card[0] in ['Jack', 'Queen', 'King']:
+        return 10
+    elif card[0] == 'Ace':
+        return 11
+    else:
+        return int(card[0])
+ 
+def get_username():
+    return input("Enter your username: ")
