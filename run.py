e2583b2c1b46b93a34e8d93ff1faf438f8acbf3d
NikkiGetsee <Nicola.getsee@gmail.com>
2024-05-03 20:02:32 +0000

    difficulty levels updated so that the return message states the actual level chosen

--- run.py
+++ run.py
@@ -20,15 +20,26 @@ def display_username(username):
     pprint(f'Welcome {username}!')
 
 def select_difficulty():
+    '''
+    function to ask user to chose a difficulty level to play the game as
+    '''
+
+    difficulty_levels = {
+        1: "Beginner",
+        2: "Intermediate",
+        3: "Advanced"
+    }
     pprint("select difficulty level:")
     pprint("1. Beginner")
     pprint("2. Intermediate")
     pprint("3. Advanced")
-    choice = input("Enter either 1,2,3:")
+    choice = input("Enter either 1,2,3: ")
     while choice not in ['1','2','3']:
         pprint("Please choose 1, 2 or 3 only")
-        choice =input("Enter either 1,2,3:")
-    pprint(f"Thank you, you chose {choice}")
-    return int(choice)
+        choice =input("Enter either 1,2,3: ")
+    level = int(choice)
+    return f"Great, you have chosen the {difficulty_levels[level]} level"
 
-select_difficulty()
+##difficulty = select_difficulty()
+##pprint(difficulty)
+
