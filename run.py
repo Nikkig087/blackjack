7064a1eb7c268df70800b967121f08d450f4a1f3
NikkiGetsee <Nicola.getsee@gmail.com>
2024-05-04 19:07:08 +0000

    working code

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import random
 from pprint import pprint 
 # Global variables
@@ -15,15 +16,12 @@ def card_value(card):
  
 def get_username():
     return input("Enter your username: ")
-
 def display_username(username):
     pprint(f'Welcome {username}!')
-
 def select_difficulty():
     '''
     function to ask user to chose a difficulty level to play the game as
     '''
-
     difficulty_levels = {
         1: "Beginner",
         2: "Intermediate",
@@ -39,10 +37,8 @@ def select_difficulty():
         choice =input("Enter either 1,2,3: ")
     level = int(choice)
     print( f"Great, you have chosen the {difficulty_levels[level]} level")
-
 ##difficulty = select_difficulty()
 ##pprint(difficulty)
-
 def display_instructions():
     '''
     Instructions on how to play the game and the rules.
@@ -56,17 +52,13 @@ def display_instructions():
     print("Should you wish to stop, the dealer (the computer) will then draw cards until its score is at least 17\n")
     print ("The player with the highest score wins!!\n")
     
-
 #display_instructions()
-
 def player_turn(deck, player_card):
     while True:
         player_score = sum(card_value(card) for card in player_card)
         print("Your card: ", player_card)
         print("Your score: ", player_score)
-
         choice = input('What do you want to do?["play" to request another card, "stop" to finish game]: ').lower()
-
         while choice not in ["play","stop"]:
             print("You must choose to either Play or Stop")
             choice = input('What do you want to do?["play" to request another card, "stop" to finish game]: ').lower()
@@ -76,7 +68,6 @@ def player_turn(deck, player_card):
             player_card.append(new_card)
         elif choice == "stop":
             break
-
         if player_score > 21:
             print("Your Cards: ", player_card)
             print("Your Score: ", player_score)
@@ -84,7 +75,6 @@ def player_turn(deck, player_card):
             return False
         
         return True 
-
 def computer_turn(deck, computer_card):
     while True:
         computer_score = sum(card_value(card) for card in computer_card)
@@ -96,7 +86,6 @@ def computer_turn(deck, computer_card):
         computer_card.append(new_card)
  
     return computer_card
-
 
 def determine_winner(player_card, computer_card):
     player_score = sum(card_value(card) for card in player_card)
@@ -146,32 +135,26 @@ def main():
     global deck
     deck = deck * deck_multiplier  #  deck size is based on difficulty level the user chose
     random.shuffle(deck)
-
     # give option to display instructions
     view_instructions = input("Would you like to view the instructions? (yes/no): ").lower()
     if view_instructions == "yes":
         display_instructions()
-
     # Initialize the cards for both the player and computer
     player_card = [deck.pop(), deck.pop()]
     computer_card = [deck.pop(), deck.pop()]
-
     # Player's turn
     player_continue = player_turn(deck, player_card)
     if not player_continue or sum(card_value(card) for card in player_card) > 21:
         # End game if player loses or goes over 21
         determine_winner(player_card, computer_card)
         return
-
     # Computer's turn
     print("Computer's turn:")
     computer_card = computer_turn(deck, computer_card)
     computer_score = sum(card_value(card) for card in computer_card)
     print("Computer's cards:", computer_card)
     print("Computer's score:", computer_score)
-
     # Determine winner
     determine_winner(player_card, computer_card)
-
 if __name__ == "__main__":
     main()
