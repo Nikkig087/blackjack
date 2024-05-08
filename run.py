580d714b0afe5e9567f50f59549303c5c0aa211c
NikkiGetsee <Nicola.getsee@gmail.com>
2024-05-08 20:29:38 +0000

    typing speed updated for type effect

--- run.py
+++ run.py
@@ -10,13 +10,13 @@ def typingPrint(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.03)
 
 def typingInput(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.03)
     value = input()  
     return value
 
